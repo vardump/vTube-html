02ed515da9e422a800578713b6e1a1faa10b118d
Hasan Misbah <hasanmisbah01@gmail.com>
2016-08-09 16:34:56 +0600

    Added sidenav and footer

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
 
     // Instantiate responsive navigation
     $(".button-collapse").sideNav();
+    $('.materialboxed').materialbox();
+    $('img').addClass('.responsive-image');
+
+    $('select').material_select();
+
+    $('.side-nav').show();
 });
